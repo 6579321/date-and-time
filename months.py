87e439cc4a9b492d69a9699ffac5f5d20f7600fd
6579321 <yasmeen.shaik99@gmail.com>
2024-11-23 13:22:12 -0500

    month

---
+++ months.py
@@ -0,0 +1,9 @@
+# List of all months
+months = [
+    "January", "February", "March", "April", "May", "June", 
+    "July", "August", "September", "October", "November", "December"
+]
+
+# Display the months
+for month in months:
+    print(month)
